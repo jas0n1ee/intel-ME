634d9c7560aac2c0a2bca16807592552c60bb325
jas0n1ee <i@jas0n1ee.me>
2014-08-22 20:19:54 +0800

    change .bat files,unimportant

---
+++ MotionEstimation/Cobra_run.bat
@@ -0,0 +1,2 @@
+x64\Release\MotionEstimation.exe --input Cobra_1920x1080_50frames.yuv --output output.yuv --width 1920 --height 1080 |grep MET
+pause
---
+++ MotionEstimation/Kimono_run.bat
@@ -0,0 +1,1 @@
+x64\Release\MotionEstimation.exe --input Kimono_1920x1080_15.yuv --output output.yuv --width 1920 --height 1080 >log.txt
